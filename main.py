90c15e8f425e19481529a9d9e653be317ca33f93
FaysalMahmudSajan <faysalmahmud4300@gmail.com>
2026-03-31 14:37:03 +0600

    "http://127.0.0.1:8080", "http://localhost:8080"

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Base.metadata.create_all(engine)
 app = FastAPI(title="Farm2Plate (Faturi)")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fatirbinabdullah.github.io","https://fatirbinabdullah.github.io/Farmer2Plate",""],
+    allow_origins=["https://fatirbinabdullah.github.io","https://fatirbinabdullah.github.io/Farmer2Plate","http://127.0.0.1:8080", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
